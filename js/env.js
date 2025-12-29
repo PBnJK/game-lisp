a8d3750c98816a9d2e88fc6e09888b550c8263d8
PBnJK <pedrobuitrago2007yahoo@gmail.com>
2025-12-29 08:56:28 -0300

    add: comments to js/env.js

--- js/env.js
+++ js/env.js
@@ -9,16 +9,19 @@ class Env {
     this.#env = new Map();
   }
 
+  /* Returns the Env map */
   getEnv() {
     return this.#env;
   }
 
+  /* Adds identifiers to the Env from an iterable Javascript object */
   addFromObject(kv) {
     for (const [k, v] of Object.entries(kv)) {
       this.setIdentifier(k, v);
     }
   }
 
+  /* Adds identifiers to the Env from another env */
   addFromEnv(env) {
     const kv = env.getEnv();
     for (const [k, v] of kv) {
@@ -26,14 +29,17 @@ class Env {
     }
   }
 
+  /* Sets the value of an identifier */
   setIdentifier(k, v) {
     this.#env.set(k, v);
   }
 
+  /* Returns the value of an identifier */
   getIdentifier(k) {
     return this.#env.get(k);
   }
 
+  /* Checks if the Env has an identifier */
   hasIdentifier(identifier) {
     return this.#env.has(identifier);
   }
